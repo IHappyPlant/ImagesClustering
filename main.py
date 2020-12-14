8e20bef7399a7e1b1955721e6a3f4dbaa6120f91
ihappyplant <karuk1998@yandex.ru>
2020-12-15 00:24:55 +0300

    added comments to code

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-# coding=utf-8
-"""
-This file contains main code for images clustering
-"""
+"""This file contains main code for images clustering"""
 import os
 from argparse import ArgumentParser
 
@@ -14,29 +11,36 @@ from sklearn.cluster import MiniBatchKMeans
 
 def main(args_):
     """Split images into clusters"""
-    model = InceptionV3(include_top=False)
+    model = InceptionV3(include_top=False)  # load model
 
     features_list = []
     files = os.listdir(args_.images)
     for f in files:
-        pth = os.path.join(args_.images, f)
+        pth = os.path.join(args_.images, f)  # get path to image
+
+        # load and preprocess image
         img = image.load_img(pth, target_size=(224, 224))
         img_data = image.img_to_array(img)
         img_data = np.expand_dims(img_data, axis=0)
         img_data = preprocess_input(img_data)
 
+        # extract features from image
         feature = np.array(model.predict(img_data))
         features_list.append(feature.flatten())
+
     features_list = np.array(features_list)
 
+    # run KMeans for extracted features
     kmeans = MiniBatchKMeans(n_clusters=args_.clusters,
                              random_state=0).fit_predict(features_list)
 
+    # generate output directory for each cluster
     output_dirs = [os.path.join(args_.destination, str(i))
                    for i in range(args_.clusters)]
     for d in output_dirs:
         os.mkdir(d)
 
+    # write files to corresponding cluster directories
     for i, file in enumerate(files):
         pth = os.path.join(args_.images, file)
         img = cv2.imread(pth)
